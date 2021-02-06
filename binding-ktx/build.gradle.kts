f623ede37bf888ff581ca80cba8cb0c8a2e91294
SorrowBlue <sorrowblue.dev@gmail.com>
2021-02-06 12:41:27 +0900

    update github action for publish

--- binding-ktx/build.gradle.kts
+++ binding-ktx/build.gradle.kts
@@ -3,67 +3,68 @@
  */
 
 plugins {
-	ComAndroidPluginGroup(this).library
-	`kotlin-android`
-	`maven-publish`
+    ComAndroidPluginGroup(this).library
+    `kotlin-android`
+    `maven-publish`
 }
 
 android {
-	compileSdkVersion(30)
-	buildToolsVersion("30.0.3")
-	defaultConfig {
-		minSdkVersion(24)
-		targetSdkVersion(30)
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.3")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(30)
 
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		consumerProguardFiles("consumer-rules.pro")
-	}
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
 
-	buildTypes {
-		val release by getting {
-			isMinifyEnabled = false
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-			)
-		}
-	}
+    buildTypes {
+        val release by getting {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
 
-	buildFeatures {
-		dataBinding = true
-		viewBinding = true
-	}
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
-	implementation(kotlin("stdlib-jdk8","1.4.30"))
-	implementation("androidx.fragment:fragment-ktx:1.3.0-rc02")
-	testImplementation("junit:junit:4.13.1")
-	androidTestImplementation("androidx.test.ext:junit:1.1.2")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    implementation(kotlin("stdlib-jdk8", "1.4.30"))
+    implementation("androidx.fragment:fragment-ktx:1.3.0-rc02")
+    testImplementation("junit:junit:4.13.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
+
 afterEvaluate {
-	publishing {
-		publications {
-			create<MavenPublication>("release") {
-				from(components.getByName("release"))
-				groupId= "com.sorrowblue.jetpack"
-				artifactId = "binding-ktx"
-				version = "1.0.1"
-			}
-		}
-		repositories {
-			maven {
-				name = "GitHubPackages"
-				url = uri("https://maven.pkg.github.com/SorrowBlue/AndroidJetpack")
-				credentials {
-					username =
-						project.findProperty("gpr.user")?.toString()
-							?: System.getenv("GITHUB_USERNAME")
-					password =
-						project.findProperty("gpr.token")?.toString()
-							?: System.getenv("GITHUB_TOKEN")
-				}
-			}
-		}
-	}
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/SorrowBlue/AndroidJetpack")
+                credentials {
+                    username = project.findProperty("gpr.user")?.toString()
+                        ?: System.getenv("GITHUB_USERNAME")
+                    password = project.findProperty("gpr.token")?.toString()
+                        ?: System.getenv("GITHUB_TOKEN")
+                    println(username)
+                    println(password)
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("release") {
+                from(components.getByName("release"))
+                groupId = "com.sorrowblue.jetpack"
+                artifactId = "binding-ktx"
+                version = "1.0.0-dev-003"
+            }
+        }
+    }
 }
