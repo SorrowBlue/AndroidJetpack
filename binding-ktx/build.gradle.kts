5f32ea38ea65dec9b4fb7b6c9fc26d39bdfe65e0
SorrowBlue <sorrowblue.dev@gmail.com>
2021-02-11 23:45:26 +0900

    Update 2.1.2

--- binding-ktx/build.gradle.kts
+++ binding-ktx/build.gradle.kts
@@ -61,7 +61,7 @@ afterEvaluate {
                 from(components.getByName("release"))
                 groupId = "com.sorrowblue.jetpack"
                 artifactId = "binding-ktx"
-                version = "2.1.1"
+                version = "2.1.2"
             }
         }
     }
